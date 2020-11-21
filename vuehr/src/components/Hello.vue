cf80671bec76f1f507558a0e59bde007c589eb3e
wangjd <1209259897@qq.com>
2020-11-21 16:48:20 +0800

    wangjd----vue整合ElementUI和Axios

---
+++ vuehr/src/components/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Hello">
+    <h1>This is an about page</h1>
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Hello"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- vuehr/src/main.js
+++ vuehr/src/main.js
@@ -3,8 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//引入Element-ui
+import Element from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
+//引入axios
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+//Vue对象使用Element-ui
+Vue.use(Element);
+//定义$axios（可以不写，在组件中直接使用axios）
+Vue.prototype.$axios= axios;
+//Vue对象使用axios
+Vue.use(axios);
 
 /* eslint-disable no-new */
 new Vue({
