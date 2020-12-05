91b4dea8f1c356fce2a3e75014934973ee70e686
wangjd <1209259897@qq.com>
2020-12-05 23:43:04 +0800

    wangjd----vue整合qs,使用axios连接后台

--- vuehr/src/main.js
+++ vuehr/src/main.js
@@ -8,6 +8,7 @@ import Element from 'element-ui'
 import "element-ui/lib/theme-chalk/index.css"
 //引入axios
 import axios from 'axios'
+import qs from 'qs'
 
 Vue.config.productionTip = false
 
@@ -17,6 +18,8 @@ Vue.use(Element);
 Vue.prototype.$axios= axios;
 //Vue对象使用axios
 Vue.use(axios);
+//配全局属性配置，在任意组件内可以使用this.$qs获取qs对象
+Vue.prototype.$qs = qs;
 
 /* eslint-disable no-new */
 new Vue({
--- vuehr/src/router/index.js
+++ vuehr/src/router/index.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'Login',
+      component: Login,
+      hidden: true
+    },
     {
       path: '/',
       name: 'Hello',
